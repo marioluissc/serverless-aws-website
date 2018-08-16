34302e1e4054e57fb42f92c776645c88d811ddc8
Mario Citadin <mario@forceadm.com>
2018-08-16 17:58:12 +1000

    Test

--- upload-code-lambda.py
+++ upload-code-lambda.py
@@ -1,17 +1,19 @@
 import boto3
 from botocore.client import Config
 import zipfile
+import mimetypes
 
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-build_bucket = s3.Bucket('portfoliobuild.robinnorwood.info')
-portfolio_bucket = s3.Bucket('portfolio.robinnorwood.info')
+build_bucket = s3.Bucket('serverless-portfolio-mario-automated')
+portfolio_bucket = s3.Bucket('serverless-portfolio-mario')
 
 # On Windows, this will need to be a different location than /tmp
-build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
+build_bucket.download_file('artifacts.zip', '/tmp/portfolio.zip')
 
 with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        target_bucket.upload_fileobj(obj, nm)
-        target_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_bucket.upload_fileobj(obj, nm,
+          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
