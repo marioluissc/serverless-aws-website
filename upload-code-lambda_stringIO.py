930a5b51139bdc753abee69914e501798d0b17cc
Mario Citadin <mario@forceadm.com>
2018-08-17 16:03:43 +1000

    Raise Exception

--- upload-code-lambda_stringIO.py
+++ upload-code-lambda_stringIO.py
@@ -4,17 +4,27 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    import boto3
+    sns = boto3.resource("sns")
+    topic = sns.Topic("arn:aws:sns:us-east-1:343947706689:DeployPortfolio")
+    try:
+            
+        s3 = boto3.resource('s3')
+        
+        portfolio_bucket = s3.Bucket('serverless-portfolio-mario')
+        build_bucket = s3.Bucket('serverless-portfolio-mario-automated')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('artifacts.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        topic.publish(Message="Portfolio Deployed - Deploy Successfully",Subject="Portfolio Deployed")
+    except Exception, e:
+        topic.publish(Message="Portfolio Deployment Failed",Subject="Portfolio Deployment Failed")
+        raise e
     
-    portfolio_bucket = s3.Bucket('serverless-portfolio-mario')
-    build_bucket = s3.Bucket('serverless-portfolio-mario-automated')
-    
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('artifacts.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
     return 'Hello From Lambda'
