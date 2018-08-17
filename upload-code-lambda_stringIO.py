558fedc59cb6e3a4eea0f44a2437d2871b18d746
Mario Citadin <mario@forceadm.com>
2018-08-17 16:51:42 +1000

    Adding CodePipeline

--- upload-code-lambda_stringIO.py
+++ upload-code-lambda_stringIO.py
@@ -7,15 +7,26 @@ def lambda_handler(event, context):
     import boto3
     sns = boto3.resource("sns")
     topic = sns.Topic("arn:aws:sns:us-east-1:343947706689:DeployPortfolio")
+    location = {
+        "bucketName" : "serverless-portfolio-mario-automated",
+        "objectKey" : "artifacts.zip"
+    }
     try:
-            
-        s3 = boto3.resource('s3')
+        job = event.get("CodePipeline.Job")
         
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+                        
+        print "Building Portfolio from " + str(location)
+        
+        s3 = boto3.resource('s3')
         portfolio_bucket = s3.Bucket('serverless-portfolio-mario')
-        build_bucket = s3.Bucket('serverless-portfolio-mario-automated')
+        build_bucket = s3.Bucket(location["bucketName"])
         
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('artifacts.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
         
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -23,6 +34,9 @@ def lambda_handler(event, context):
                 portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         topic.publish(Message="Portfolio Deployed - Deploy Successfully",Subject="Portfolio Deployed")
+        if job:
+            codePipeline = boto3.client('codepipeline')
+            codePipeline.put_job_success_result(jobId = job["Id"])
     except Exception, e:
         topic.publish(Message="Portfolio Deployment Failed",Subject="Portfolio Deployment Failed")
         raise e
